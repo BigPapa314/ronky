535ba8c019a89fcf31276389bd5d0640b89d6ccc
Thomas Kilian <big_papa@gmx.net>
2022-12-31 16:07:53 +0100

    Added text label

--- ronky-demo/src/main.rs
+++ ronky-demo/src/main.rs
@@ -1,5 +1,39 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Horst".into())));
+    commands.spawn((Person, Name("Egon".into())));
+    commands.spawn((Person, Name("Hans".into())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
-    ronky::main();
+    ronky::run(HelloPlugin);
 }
--- ronky/src/lib.rs
+++ ronky/src/lib.rs
@@ -1,4 +1,26 @@
+use bevy::prelude::*;
 
-pub fn main() {
-    println!("Hello, world from ronky!");
+pub fn run<T: Plugin>(configuration: T) {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_plugin(configuration)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    let text_style = TextStyle {
+        font,
+        font_size: 60.0,
+        color: Color::WHITE,
+    };
+    let text_alignment = TextAlignment::CENTER;
+    // 2d camera
+    commands.spawn(Camera2dBundle::default());
+    // Demonstrate changing translation
+    commands.spawn(Text2dBundle {
+        text: Text::from_section("translation", text_style.clone()).with_alignment(text_alignment),
+        ..default()
+    });
 }
